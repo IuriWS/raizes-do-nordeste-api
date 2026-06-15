8e566a671cf5935eccc950b233923655368dae1c
IuriWS <IuriWS@users.noreply.github.com>
2026-06-15 18:46:44 -0400

    criando estrutura inicial do projeto

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/pacote/', '/test/.*\\.e2e-spec\\.ts$'],
+  modulePathIgnorePatterns: ['<rootDir>/pacote/'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  testEnvironment: 'node'
+};
+
+export default config;
---
+++ jest.e2e.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: 'test/.*\\.e2e-spec\\.ts$',
+  testPathIgnorePatterns: ['/node_modules/', '/pacote/'],
+  modulePathIgnorePatterns: ['<rootDir>/pacote/'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  maxWorkers: 1
+};
+
+export default config;
